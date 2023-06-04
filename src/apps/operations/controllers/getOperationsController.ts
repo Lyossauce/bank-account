fc2a18acb0dc31ee83b3f3a2672b300418c869ce
JMercierC <lyossos2.0@gmail.com>
2023-06-04 13:33:58 +0200

    add serverless config and getOperations

---
+++ src/apps/operations/controllers/getOperationsController.ts
@@ -0,0 +1,10 @@
+import { APIGatewayProxyEvent } from 'aws-lambda';
+
+/**
+ * @name getOperationsController
+ * @description Get list of operations sorted by date
+ * @param {APIGatewayProxyEvent} request
+ *
+ */
+export const getOperationsController = async (request: APIGatewayProxyEvent) => {
+};
