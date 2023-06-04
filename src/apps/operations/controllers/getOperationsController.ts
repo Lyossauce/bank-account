a4ae780b28b1756a6cf9d49b5da900af4338faa6
JMercierC <lyossos2.0@gmail.com>
2023-06-04 13:39:00 +0200

    add getOperations Controller

--- src/apps/operations/controllers/getOperationsController.ts
+++ src/apps/operations/controllers/getOperationsController.ts
@@ -1,4 +1,7 @@
+import { AccountRepository } from '../../helpers/repositories/AccountRepository';
 import { APIGatewayProxyEvent } from 'aws-lambda';
+import { OperationDbRecord } from '../../../models/DBRecords';
+import { OperationRepository } from '../../helpers/repositories/OperationRepository';
 
 /**
  * @name getOperationsController
@@ -7,4 +10,29 @@ import { APIGatewayProxyEvent } from 'aws-lambda';
  *
  */
 export const getOperationsController = async (request: APIGatewayProxyEvent) => {
+  const accountId = (request.pathParameters as Record<string, any>).accountId;
+
+  const account = await AccountRepository.getOne(accountId);
+
+  if (!account) {
+    return {
+      statusCode: 404,
+      body: JSON.stringify({
+        message: 'ACCOUNT NOT FOUND',
+      }),
+    };
+  }
+
+  const operations: OperationDbRecord[] = await OperationRepository.getByAccountId(accountId);
+
+  return {
+    results: operations.map((operation: OperationDbRecord) => {
+      return {
+        date: operation.date,
+        amount: operation.amount,
+        balance: operation.balance,
+        type: operation.type,
+      };
+    }),
+  };
 };
